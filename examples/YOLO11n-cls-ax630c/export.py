c89bf7fb6d6c3211e47c8dc1b53814066a9b2606
Jordan-5i <790712086@qq.com>
2025-10-22 16:43:02 +0800

    add yolov5-seg.yaml

--- examples/YOLO11n-cls-ax630c/export.py
+++ examples/YOLO11n-cls-ax630c/export.py
@@ -3,5 +3,8 @@ from ultralytics import YOLO
 # Load a model
 model = YOLO("yolo11n-cls.pt")  # load an official model
 
+# or load from a yaml file e.g. "yolov8n-seg.yaml"
+# model = YOLO("yolov8m-seg.yaml")
+
 # Export the model
 model.export(format="onnx", device="cpu")
